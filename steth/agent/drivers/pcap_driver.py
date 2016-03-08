a981d2eff81eec01d3663c5530e79852619e9d7e
changzhi1990 <changzhi@unitedstack.com>
2016-03-08 18:08:01 +0800

    Change "pcap" to "pcapy"
    In CentOS 7, there is no rpm named "pcap" rather than
    "pcapy".
    
    Change-Id: I04b78201e6a2d797a8d5655fab1bc042a9976431

--- steth/agent/drivers/pcap_driver.py
+++ steth/agent/drivers/pcap_driver.py
@@ -13,7 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import pcap
+import pcapy
 from steth.agent.common import log
 from steth.agent.common import utils
 from steth.agent.common import constants
@@ -24,7 +24,7 @@ LOG = log.get_logger()
 class PcapDriver(object):
 
     def setup_listener(self, iface, filter, timeout=2):
-        listener = pcap.pcap(iface, timeout_ms=timeout * 1000)
+        listener = pcapy.pcapy(iface, timeout_ms=timeout * 1000)
         listener.setfilter(filter)
         return listener
 
