c9a0da43a2f4bb44b2ac77dcd237fd42c4008da6
changzhi <changzhi@unitedstack.com>
2016-08-04 17:29:37 +0800

    Refactor read config file
    In order to make configuration more clearly,
    seperating "network_types" configuration into serveral configuration.
    
    Add timeout decorator to make sure program can goes more smoothly.
    
    Change-Id: I0d9765b23f89e2c8ac8b1d9047cfebb89f04df8d

--- steth/stethclient/utils.py
+++ steth/stethclient/utils.py
@@ -13,8 +13,12 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import errno
+from functools import wraps
 import logging
 import jsonrpclib
+import os
+import signal
 import six
 import socket
 import sys
@@ -156,3 +160,26 @@ def is_uuid_like(val):
         return str(uuid.UUID(val)).replace('-', '') == _format_uuid_string(val)
     except (TypeError, ValueError, AttributeError):
         return False
+
+
+class TimeoutError(Exception):
+    pass
+
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
